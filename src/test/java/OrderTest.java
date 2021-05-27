16f0284ad357d416de7874f7233ac59195104ebf
Екатерина Сочкова <volontare@yandex.ru>
2021-05-27 19:58:06 +0400

    Fix1

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.openqa.selenium.By.cssSelector;
 
 
 class OrderTest {
@@ -21,12 +22,14 @@ class OrderTest {
     }
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+        Thread.sleep(5000);
     }
 
     @AfterEach
@@ -36,70 +39,58 @@ class OrderTest {
     }
 
     @Test
-    void shouldTestOrderIfValidData() throws InterruptedException {
-        driver.get("http://localhost:9999");
-        Thread.sleep(5000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Евгения Смирнова");
-        elements.get(1).sendKeys("+79991234567");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+    void shouldTestOrderIfValidData() {
+        WebElement form = driver.findElement(cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Евгения Смирнова");
+        form.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79991234567");
+        form.findElement(cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(cssSelector("[type=button]")).click();
+        String text = driver.findElement(cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
-    void shouldTestIfIncorrectTel() throws InterruptedException {
-        driver.get("http://localhost:9999");
-        Thread.sleep(5000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Евгения Смирнова");
-        elements.get(1).sendKeys("89991234567");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        elements = driver.findElements(By.className("input__sub"));
-        String text = elements.get(1).getText();
-        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    void shouldTestIfIncorrectTel() {
+        WebElement form = driver.findElement(cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Евгения Смирнова");
+        form.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("89991234567");
+        form.findElement(cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(cssSelector("[type=button]")).click();
+        assertTrue(!form.findElements(cssSelector("[class='input input_type_tel input_view_default input_size_m input_width_available" +
+                " input_has-label input_has-value input_invalid input_theme_alfa-on-white']")).isEmpty());
     }
 
     @Test
-    void shouldTestIfIncorrectName() throws InterruptedException {
-        driver.get("http://localhost:9999");
-        Thread.sleep(5000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Evgeniya Smirnova");
-        elements.get(1).sendKeys("+79876543210");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        elements = driver.findElements(By.className("input__sub"));
-        String text = elements.get(0).getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    void shouldTestIfIncorrectName() {
+        WebElement form = driver.findElement(cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Evgeniya Smirnova");
+        form.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79991234567");
+        form.findElement(cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(cssSelector("[type=button]")).click();
+        assertTrue(!form.findElements(cssSelector("[class='input input_type_text input_view_default input_size_m input_width_available " +
+                "input_has-label input_has-value input_invalid input_theme_alfa-on-white']")).isEmpty());
     }
 
     @Test
-    void shouldTestIfNoName() throws InterruptedException {
-        driver.get("http://localhost:9999");
-        Thread.sleep(5000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(1).sendKeys("+79991234567");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        elements = driver.findElements(By.className("input__sub"));
+    void shouldTestIfNoName() {
+        WebElement form = driver.findElement(By.cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79991234567");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[type=button]")).click();
+        List<WebElement> elements = driver.findElements(cssSelector("[class=input__sub]"));
         String text = elements.get(0).getText();
         assertEquals("Поле обязательно для заполнения", text.trim());
     }
 
     @Test
-    void shouldTestIfNoTel() throws InterruptedException {
-        driver.get("http://localhost:9999");
-        Thread.sleep(5000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Евгения Смирнова");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        elements = driver.findElements(By.className("input__sub"));
+    void shouldTestIfNoTel() {
+        WebElement form = driver.findElement(By.cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Евгения Смирнова");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[type=button]")).click();
+        List<WebElement> elements = driver.findElements(cssSelector("[class=input__sub]"));
         String text = elements.get(1).getText();
         assertEquals("Поле обязательно для заполнения", text.trim());
     }
-    
+
 }
